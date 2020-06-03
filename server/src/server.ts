1990bd2a92c16b892c981bb4e384e34d08de5846
Lorenalgm <lorena@proesc.com>
2020-06-03 00:21:15 -0300

    Added users routes

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,10 +2,40 @@ import express from 'express';
 
 const app = express();
 
-app.get('/users', (request, response) => {
-    console.log('Listagem');
+app.use(express.json());
 
-    response.json([ 'lORENaaaaaaaaaA', 'teste']);
+const users = [
+    'lorena',
+    'luma',
+    'luana'
+]
+
+app.get('/users', (request, response) => {
+    const search = String(request.query.search);
+
+    const filteredUsers = search ? users.filter(user => user.includes(search)) : users;
+
+    return response.json(filteredUsers);
+});
+
+app.get('/users/:id', (request, response) => {
+    const id = Number(request.params.id);
+
+    const user = users[id];
+
+    return response.json(user);
+});
+
+
+app.post('/users', (request, response) => {
+    const data = request.body;
+    
+    const user = {
+        name: data.name,
+        email: data.email,
+    };
+
+    return response.json(user);
 });
 
 app.listen(3333);
